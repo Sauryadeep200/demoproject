62e86c7845936a8882b3c0a1f90f3c155a77d5b4
Sauryadeep200 <sauryadeeproufcc60@gmail.com>
2021-10-07 07:01:34 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+ublic class multiply {
+
+    public static void main(String[] args) {
+         
+       int num1 = 5, num2 = 15, mul;
+       mul = num1 + num2;
+ 
+       System.out.println("mul of  numbers: "+mul);
+    }
+ }
